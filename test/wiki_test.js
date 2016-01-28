fccf3b79b0c1814d1ae108c0ffd51ab3d6705ca5
Emma Bishop <emma.b.bishop@gmail.com>
2016-01-28 14:12:33 -0500

    Adds tests for findByTags

--- test/wiki_test.js
+++ test/wiki_test.js
@@ -4,34 +4,44 @@ expect = chai.expect;
 
 
 var Page = require('../models').Page;
-
+require('../models');
 
 describe('Page model', function(){
-	var emptyPage, myPage;
-	beforeEach(function(){
-		emptyPage = new Page(); //entirely synchronous because it doesn't need to access database
-	});
-
-	beforeEach(function(done){
+		//set up empty page
+		var emptyPage, myPage;
+		beforeEach(function(){
+			emptyPage = new Page(); //entirely synchronous because it doesn't need to access database
+		});
+		//set up page with title & content
+		beforeEach(function(done){
 		Page.create({
 			title: '1601',
-			content: '1601 is great'
+			content: '1601 is great',
+			tags: ['todayisthursday', 'gracehopper', 'werethebest']
 		})
 		.then(function(page){
 			myPage = page;
 			done(); // done is necessary because the test needs to access the database
+			});
 		});
 
-	});
+		afterEach(function(done){
+			myPage.remove({})
+			.then(function(){
+				done();
+			})
+			.then(null, done);
+		});
+
+	describe('Validations', function(){
 	
-	describe('validations', function(){
+		
 		it('exptect title to be required', function(done){
 			emptyPage.validate(function(err){
 				expect(err.errors).to.have.property('title');
 				done();
 			})
 		});
-		
 		it('expect content to be required', function(done){
 			emptyPage.validate(function(err){
 				expect(err.errors).to.have.property('content');
@@ -43,8 +53,22 @@ describe('Page model', function(){
 
     describe('Statics', function() {
         describe('findByTag', function() {
-            xit('gets pages with the search tag', function() {});
-            xit('does not get pages without the search tag', function() {});
+            it('gets pages with the search tag', function(done) {
+            	Page.findByTag('todayisthursday') //WHAAAAT?
+            	.then(function(pages){
+            		console.log(pages.length);
+	            	expect(pages).to.have.lengthOf(1);
+	            	done();
+            	})
+            	.then(null, done);
+            });
+            it('does not get pages without the search tag', function(done) {
+			    Page.findByTag('falafel')
+			    .then(function (pages) {
+			        expect(pages).to.have.lengthOf(0)
+			        done();
+			    }).then(null, done);
+			})
         });
     });
 
